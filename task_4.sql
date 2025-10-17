240ab934ca7ae402cdf35b8036960428056dea39
Nouh <noohahmed600@gmail.com>
2025-10-17 20:11:10 +0300

    Added task_4.sql and added script to select full description of the books table

--- task_4.sql
+++ task_4.sql
@@ -1,11 +1,14 @@
 -- Script for books full description
 USE alx_book_store;
 
--- Show the complete CREATE statement for the books table
-SHOW CREATE TABLE books;
-
-
-SHOW COLUMNS FROM books;
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY 
+-- Show the full description of the books table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_NAME = 'books' AND TABLE_SCHEMA = 'alx_book_store';
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store'
+ORDER BY ORDINAL_POSITION;
