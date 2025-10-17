89419d81123181d589424e6f0527f4882123752a
Nouh <noohahmed600@gmail.com>
2025-10-17 19:41:07 +0300

    Added task_2.sql and created some tables

---
+++ task_2.sql
@@ -0,0 +1,46 @@
+-- Create database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+
+-- Table to store authors information
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Table to store customers information
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT NOT NULL
+);
+
+-- Table to store books information
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    published_date DATE,
+    price DECIMAL(10,2),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Table to store orders information
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Table to store order details
+CREATE TABLE Order_Details(
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
